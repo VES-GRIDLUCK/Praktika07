033539ef6bf79f54803a0ade14e8eee4f9549c9d
Егор Васин <egor.vasin1011@gmail.com>
2023-07-03 19:30:15 +0300

    7

--- src/test7.java
+++ src/test7.java
@@ -4,8 +4,9 @@ public class test7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        char s = scanner.next().charAt(0);
         for(int i=0; i <n; i++){
-            System.out.println("%");
+            System.out.println(s);
         }
     }
 }
